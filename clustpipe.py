6d0525918720f12db8b87c1332efb8d1f7436e36
Remi Adam <adam.remi.gm@gmail.com>
2020-03-04 14:46:50 +0100

    admin -> common

--- clustpipe.py
+++ clustpipe.py
@@ -13,33 +13,26 @@ is available. The class provides an analysis pipeline to reduce the events.
 import numpy as np
 import astropy.units as u
 from astropy.coordinates.sky_coordinate import SkyCoord
-import gammalib
 
 from ClusterModel      import model          as model_cluster
 from ClusterPipe.Tools import compact_source as model_compsource
 from ClusterPipe.Tools import obs_setup      as setup_observations
-from ClusterPipe.Tools import make_cluster_template
-from ClusterPipe.Tools import build_ctools_model
-from ClusterPipe.Tools import utilities
-from ClusterPipe.clustpipe_admin import Admin
-from ClusterPipe.clustpipe_sim   import CTAsim
-from ClusterPipe.clustpipe_ana   import CTAana
+from ClusterPipe.clustpipe_common import Common
+from ClusterPipe.clustpipe_sim    import CTAsim
+from ClusterPipe.clustpipe_ana    import CTAana
 from ClusterPipe import clustpipe_title
 
 #==================================================
 # ClusterPipe class
 #==================================================
 
-class ClusterPipe(Admin, CTAsim, CTAana):
+class ClusterPipe(Common, CTAsim, CTAana):
     """ 
     ClusterPipe class. 
     This class defines a ClusterPipe object. This contain the sky model, as well
     as the observation setup. It can generate event files given the model and 
     provide an analysis pipeline to extract relevant information.
-    
-    To do list
-    ----------
-    
+        
     Attributes
     ----------  
     - silent (bool): print information if False, or not otherwise.
@@ -54,6 +47,7 @@ class ClusterPipe(Admin, CTAsim, CTAana):
     
     Methods
     ----------  
+    - See Common, CTAsim, and CTAana sub-classes
     
     """
     
@@ -97,17 +91,17 @@ class ClusterPipe(Admin, CTAsim, CTAana):
         self.obs_setup = obs_setup
         
         #---------- Analysis parameters
-        # Map related
-        self.map_reso     = 0.02*u.deg
-        self.map_coord    = SkyCoord(0.0, 0.0, frame="icrs", unit="deg")
-        self.map_fov      = 10*u.deg
-        
         # Likelihood method related
         self.method_stack  = True
         self.method_binned = False
         self.method_stat   = 'DEFAULT' # CSTAT, WSTAT, CHI2
         self.method_onoff  = False
 
+        # Map related
+        self.map_reso  = 0.02*u.deg
+        self.map_coord = SkyCoord(0.0, 0.0, frame="icrs", unit="deg")
+        self.map_fov   = 10*u.deg
+        
         # Spectrum related
         self.spec_edisp    = False
         self.spec_ebinalg  = 'LOG'
@@ -119,79 +113,4 @@ class ClusterPipe(Admin, CTAsim, CTAana):
         self.time_tmin     = None
         self.time_tmax     = None
         self.time_phase    = None
-        
-        
-    #==================================================
-    # Load the simulation configuration
-    #==================================================
-    
-    def _match_cluster_to_pointing(self, extra=1.1):
-        """
-        Match the cluster map and according to the pointing list.
-        
-        Parameters
-        ----------
-        - extra (float): factor to apply to the cluster extent to 
-        have a bit of margin on the side of the map
-
-        Outputs
-        -------
-        The cluster map properties are modified
-        
-        """
-
-        # Compute the pointing barycenter and the FoV requested size 
-        list_ptg_coord = SkyCoord(self.obs_setup.coord)
-        list_ptg_rad   = self.obs_setup.rad
-        center_ptg, fov_ptg = utilities.listcord2fov(list_ptg_coord, list_ptg_rad)
-
-        # Account for the cluster
-        fov = utilities.squeeze_fov(center_ptg, fov_ptg,
-                                    self.cluster.coord, self.cluster.theta_truncation,
-                                    extra=extra)
-
-        # Set the cluster map to match the pointing
-        self.cluster.map_coord = center_ptg
-        self.cluster.map_fov   = fov
-        
-        
-    #==================================================
-    # Make a model
-    #==================================================
-    
-    def _make_model(self, prefix='Model', includeIC=True):
-        """
-        This function is used to construct the model.
-        
-        Parameters
-        ----------
-        - prefix (str): text to add as a prefix of the file names
-        - includeIC (bool): include inverse Compton in the model
-        
-        """
-        
-        #----- Make cluster template files
-        if (not self.silent) and (self.cluster.map_reso > 0.02*u.deg):
-            print('WARNING: the FITS map resolution is larger than 0.02 deg, i.e. the PSF at 100 TeV')
-        
-        make_cluster_template.make_map(self.cluster,
-                                       self.output_dir+'/'+prefix+'Map.fits',
-                                       Egmin=self.obs_setup.get_emin(),
-                                       Egmax=self.obs_setup.get_emax(),
-                                       includeIC=includeIC)
-        
-        make_cluster_template.make_spectrum(self.cluster,
-                                            self.output_dir+'/'+prefix+'Spectrum.txt',
-                                            energy=np.logspace(-1,5,1000)*u.GeV,
-                                            includeIC=includeIC)
-        
-        #----- Create the model
-        model_tot = gammalib.GModels()
-        build_ctools_model.cluster(model_tot,
-                                   self.output_dir+'/'+prefix+'Map.fits',
-                                   self.output_dir+'/'+prefix+'Spectrum.txt',
-                                   ClusterName=self.cluster.name)
-        build_ctools_model.compact_sources(model_tot, self.compact_source)
-        build_ctools_model.background(model_tot, self.obs_setup.bkg)
-        model_tot.save(self.output_dir+'/'+prefix+'.xml')
         
